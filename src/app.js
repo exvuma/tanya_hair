f1a2ec6432dd93138123361379a69b98774b7ea5
Victoria <bernard.victoria22@gmail.com>
2018-09-19 20:12:36 -0500

    webpack working with scrollmagic

--- src/app.js
+++ src/app.js
@@ -2,6 +2,21 @@
 // var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
 // var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
 // console.log(rightNow);
-console.log('sdf324');
+let ScrollMagic = require('scrollmagic')
+let TweenMax = require('gsap/TweenMax').TweenMax
+// console.log('sdf324');
 
 // "October 23rd 2016, 9:30:24 pm"
+var controller = new ScrollMagic.Controller();
+var blockTween = new TweenMax.to("#ani-img", 1.5, {
+    backgroundColor: "red",
+    width:"5%",
+    height: "auto",
+    
+});
+var scene = new ScrollMagic.Scene({
+    triggerElement: '#ani-img'
+
+})
+    .setTween
+    .addTo(controller)
--- webpack.config.js
+++ webpack.config.js
@@ -4,30 +4,30 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 var LiveReloadPlugin = require('webpack-livereload-plugin');
 var options = {}
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
+  context: path.resolve(__dirname, "src"),
   entry: {
-    app: './app.js',
+    app: __dirname + '/src/app.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html'
+      filename: "index.html",
+      template: "index.html",
+      inject: "body"
     }),
-     new LiveReloadPlugin(options),
+    new LiveReloadPlugin(options),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin()
   ],
   output: {
-    path: path.resolve(__dirname, 'dist', 'assets'),
-    publicPath: '/assets',                          // New
-    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, "dist", "assets"),
+    // publicPath: "/assets", // New
+    filename: "[name].bundle.js"
   },
   devServer: {
-    
-      contentBase: path.resolve(__dirname, 'src'),    // New
-      // contentBase: path.join(__dirname, 'dist'),
-      compress: true,
+    // contentBase: path.resolve(__dirname, 'src'),    // New
+    // contentBase: path.join(__dirname, 'dist'),
+    compress: true,
     port: 9000,
     hot: true
-  },
+  }
 };
